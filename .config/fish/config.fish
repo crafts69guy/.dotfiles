c62d2a5c10a20c8aabb691885ac25b66f891c9c2
insideee.dev <insideee.dev@gmail.com>
2025-02-20 12:12:42 +0700

    chore: add .config and .czrc

---
+++ .config/fish/config.fish
@@ -0,0 +1,80 @@
+if status is-interactive
+    # Run home brew
+    set -gx PATH /opt/homebrew/bin $PATH
+
+    set -x NVM_DIR ~/.local/share/nvm
+    set -x NODE_VERSION v20.14.0
+    set -x PATH $NVM_DIR/$NODE_VERSION/bin $PATH
+
+    set fish_greeting ""
+
+    set -gx TERM tmux-256color
+
+    # aliases
+    alias ls "ls -p -G"
+    alias la "ls -A"
+    alias ll "ls -l"
+    alias lla "ll -A"
+    alias g git
+    # alias ga git add
+    # alias gc git commit
+    # alias gp git push
+    command -qv nvim && alias vim nvim
+
+    set -gx EDITOR nvim
+
+    set -gx PATH bin $PATH
+    set -gx PATH ~/bin $PATH
+    set -gx PATH ~/.local/bin $PATH
+
+    # Commands to run in interactive sessions can go here
+    set -g theme_display_git no
+    set -g theme_display_git_dirty no
+    set -g theme_display_git_untracked no
+    set -g theme_display_git_ahead_verbose yes
+    set -g theme_display_git_dirty_verbose yes
+    set -g theme_display_git_stashed_verbose yes
+    set -g theme_display_git_default_branch yes
+    set -g theme_git_default_branches master main
+    set -g theme_git_worktree_support yes
+    set -g theme_use_abbreviated_branch_name yes
+    set -g theme_display_vagrant yes
+    set -g theme_display_docker_machine no
+    set -g theme_display_k8s_context yes
+    set -g theme_display_hg yes
+    set -g theme_display_virtualenv no
+    set -g theme_display_nix no
+    set -g theme_display_ruby no
+    set -g theme_display_node yes
+    set -g theme_display_user ssh
+    set -g theme_display_hostname ssh
+    set -g theme_display_vi yes
+    set -g theme_display_date no
+    set -g theme_display_cmd_duration yes
+    set -g theme_title_display_process yes
+    set -g theme_title_display_path no
+    set -g theme_title_display_user yes
+    set -g theme_title_use_abbreviated_path no
+    set -g theme_date_format "+%a %H:%M"
+    set -g theme_date_timezone America/Los_Angeles
+    set -g theme_avoid_ambiguous_glyphs no
+    set -g theme_powerline_fonts yes
+    set -g theme_nerd_fonts yes
+    set -g theme_show_exit_status yes
+    set -g theme_display_jobs_verbose yes
+    set -g default_user your_normal_user
+    set -g theme_color_scheme nord
+    set -g fish_prompt_pwd_dir_length 0
+    set -g theme_project_dir_length 1
+    set -g theme_newline_cursor yes
+    set -g theme_newline_prompt '$ '
+
+    switch (uname)
+        case Darwin
+            source (dirname (status --current-filename))/config-osx.fish
+        case Linux
+            source (dirname (status --current-filename))/config-linux.fish
+        case '*'
+            source (dirname (status --current-filename))/config-windows.fish
+    end
+end
---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,184 @@
+return {
+	-- Incremental rename
+	{
+		"smjonas/inc-rename.nvim",
+		cmd = "IncRename",
+		config = true,
+	},
+
+	-- Refactoring tool
+	{
+		"ThePrimeagen/refactoring.nvim",
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("refactoring").select_refactor()
+				end,
+				mode = "v",
+				noremap = true,
+				silent = true,
+				expr = false,
+			},
+		},
+		opts = {},
+	},
+
+	-- Go forward/backward with square brackets
+	{
+		"echasnovski/mini.bracketed",
+		event = "BufReadPost",
+		config = function()
+			local bracketed = require("mini.bracketed")
+			bracketed.setup({
+				file = { suffix = "" },
+				window = { suffix = "" },
+				quickfix = { suffix = "" },
+				yank = { suffix = "" },
+				treesitter = { suffix = "n" },
+			})
+		end,
+	},
+
+	-- Better increase/descrease
+	{
+		"monaqa/dial.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    },
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.alias.bool,
+					augend.semver.alias.semver,
+					augend.constant.new({ elements = { "let", "const" } }),
+				},
+			})
+		end,
+	},
+
+	{
+		"simrat39/symbols-outline.nvim",
+		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+		cmd = "SymbolsOutline",
+		opts = {
+			position = "right",
+		},
+	},
+
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			{
+				"codeium.nvim",
+				"saghen/blink.compat",
+				"rafamadriz/friendly-snippets",
+			},
+		},
+		opts = {
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+
+			signature = {
+				enabled = true,
+				window = {
+					winblend = vim.o.pumblend,
+				},
+			},
+
+			appearance = {
+				use_nvim_cmp_as_default = false,
+				-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = true,
+					},
+				},
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+					},
+					winblend = vim.o.pumblend,
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = vim.g.ai_cmp,
+				},
+			},
+
+			sources = {
+				-- adding any nvim-cmp sources here will enable them
+				-- with blink.compat
+				compat = { "codeium" },
+				default = { "lsp", "path", "snippets", "buffer" },
+
+				-- add lazydev to your completion providers
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100, -- show at a higher priority than lsp
+					},
+					codeium = {
+						kind = "Codeium",
+						score_offset = 100,
+						async = true,
+					},
+				},
+			},
+
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
+		},
+	},
+
+	{
+		"xzbdmw/colorful-menu.nvim",
+		config = function() end,
+	},
+
+	{
+		"saghen/blink.compat",
+		optional = true, -- make optional so it's only enabled if any extras need it
+		lazy = true,
+		opts = {},
+		version = not vim.g.lazyvim_blink_main and "*",
+	},
+
+	{
+		"catppuccin",
+		optional = true,
+		opts = {
+			integrations = { blink_cmp = true },
+		},
+	},
+}
