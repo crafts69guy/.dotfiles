8f1d27624fff1c8cfa9602fdd843af71f4af1709
insideee.dev <insideee.dev@gmail.com>
2025-02-25 09:54:54 +0700

    chore: move .czrc on level up

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,13 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
+			lua = { "stylua" },
+			["javascript"] = { "biome-check" },
+			["javascriptreact"] = { "biome-check" },
+			["typescript"] = { "biome-check" },
+			["typescriptreact"] = { "biome-check" },
+			["json"] = { "biome-check" },
+			["css"] = { "biome-check" },
 			["markdown"] = { "prettier" },
 		},
 	},
