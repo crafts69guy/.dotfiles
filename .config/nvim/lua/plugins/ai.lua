4368b74ee590f00513d2ce5b84e4235385ab8d8d
insideee.dev <insideee.dev@gmail.com>
2025-10-14 14:19:02 +0700

    chore: disable nes

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -9,19 +9,20 @@ return {
 					enabled = true,
 				},
 			},
+			nes = { enabled = false },
 		},
 		keys = {
-			{
-				"<leader>A",
-				function()
-					-- if there is a next edit, jump to it, otherwise apply it if any
-					if not require("sidekick").nes_jump_or_apply() then
-						return "<Tab>" -- fallback to normal tab
-					end
-				end,
-				expr = true,
-				desc = "Goto/Apply Next Edit Suggestion",
-			},
+			-- {
+			-- 	"<leader>A",
+			-- 	function()
+			-- 		-- if there is a next edit, jump to it, otherwise apply it if any
+			-- 		if not require("sidekick").nes_jump_or_apply() then
+			-- 			return "<Tab>" -- fallback to normal tab
+			-- 		end
+			-- 	end,
+			-- 	expr = true,
+			-- 	desc = "Goto/Apply Next Edit Suggestion",
+			-- },
 			{
 				"<c-.>",
 				function()
