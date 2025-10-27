4cda409c1ac8a8a897093739c18f576974b6c526
insideee.dev <insideee.dev@gmail.com>
2025-10-27 11:29:27 +0700

    refactor(nvim): make floating windows and UI popups transparent
    Configure NormalFloat, FloatBorder, Telescope, and Which-key highlight groups
    to have transparent backgrounds. This allows floating windows and popups to
    blend seamlessly with the editor background instead of showing a colored box,
    creating a cleaner, more integrated appearance with the solarized-osaka theme.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,14 +1,51 @@
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+	pattern = "*",
+	command = "set nopaste",
 })
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
-  callback = function()
-    vim.opt.conceallevel = 0
-  end,
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
 })
+
+-- Make floating windows, telescope, and which-key transparent with themed borders
+local function set_transparent_floats()
+	vim.schedule(function()
+		-- Get solarized-osaka colors
+		local colors = require("solarized-osaka.colors").default
+		local border_color = colors.cyan
+
+		-- Floating windows
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { fg = border_color, bg = "NONE" })
+
+		-- Telescope
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = border_color, bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = border_color, bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = border_color, bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = border_color, bg = "NONE" })
+
+		-- Which-key
+		vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "WhichKeyBorder", { fg = border_color, bg = "NONE" })
+		vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "WhichKeyTitle", { bg = "NONE" })
+	end)
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = set_transparent_floats,
+})
+
+-- Also apply on startup
+set_transparent_floats()
