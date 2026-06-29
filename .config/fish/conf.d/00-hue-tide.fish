b2ae9bf56374799c322a2585d18be1fe7fa498e1
crafts69.guy <crafts69.guy@gmail.com>
2026-06-29 17:12:40 +0700

    Vendor Hue Tide fish theme

--- .config/fish/conf.d/00-hue-tide.fish
+++ .config/fish/conf.d/00-hue-tide.fish
@@ -1,3 +1,9 @@
 # Apply generated Hue colors to Tide without writing theme values to fish_variables.
 set -g hue_flavour mua
-source /Users/caongoccuong/Developments/github.com/crafts69guy/hue-theme/packages/fish-themes/tide/hue.fish
+
+set -l hue_tide_entry (dirname (status filename))/../themes/hue/tide/hue.fish
+if test -f $hue_tide_entry
+    source $hue_tide_entry
+else
+    echo "hue-tide: missing theme entrypoint at $hue_tide_entry" >&2
+end
