c8d4b8ddd12e880ca2e1ac45612a64e35ee8eed0
crafts69.guy <crafts69.guy@gmail.com>
2026-06-29 17:08:46 +0700

    Apply Hue Tide fish theme

---
+++ .config/fish/conf.d/00-hue-tide.fish
@@ -0,0 +1,3 @@
+# Apply generated Hue colors to Tide without writing theme values to fish_variables.
+set -g hue_flavour mua
+source /Users/caongoccuong/Developments/github.com/crafts69guy/hue-theme/packages/fish-themes/tide/hue.fish
