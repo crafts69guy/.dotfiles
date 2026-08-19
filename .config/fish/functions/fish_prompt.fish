cadb30d11f7ddb93ddb9d6df3c30a8ef556963f5
crafts69.guy <crafts69.guy@gmail.com>
2026-08-19 10:02:06 +0700

    chore(fish): upgrade Tide to 6.2.0

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -11,9 +11,7 @@ set -l prompt_var _tide_prompt_$fish_pid
 set -U $prompt_var # Set var here so if we erase $prompt_var, bg job won't set a uvar
 
 set_color normal | read -l color_normal
-# Keep the generated prompt valid across Homebrew upgrades. `status fish-path`
-# resolves to a versioned Cellar path that disappears when the old keg is cleaned.
-command --search fish | read -l fish_path
+status fish-path | read -l fish_path
 
 # _tide_repaint prevents us from creating a second background job
 function _tide_refresh_prompt --on-variable $prompt_var --on-variable COLUMNS
@@ -30,7 +28,7 @@ if contains newline $_tide_left_items # two line prompt initialization
     test "$tide_left_prompt_frame_enabled" = true &&
         set -l top_left_frame "$prompt_and_frame_color╭─" &&
         set -l bot_left_frame "$prompt_and_frame_color╰─" &&
-        set column_offset (math $column_offset-2)
+        set column_offset 3
     test "$tide_right_prompt_frame_enabled" = true &&
         set -l top_right_frame "$prompt_and_frame_color─╮" &&
         set -l bot_right_frame "$prompt_and_frame_color─╯" &&
@@ -147,21 +145,20 @@ end"
     end
 end
 
-eval "function _tide_on_fish_exit --on-event fish_exit
+# Inheriting instead of evaling because here load time is more important than runtime
+function _tide_on_fish_exit --on-event fish_exit --inherit-variable prompt_var
     set -e $prompt_var
-end"
+end
 
 if test "$tide_prompt_transient_enabled" = true
     function _tide_enter_transient
-        # If the commandline will be executed, or is empty
-        if commandline --is-valid || test -z "$(commandline)"
-            # Pager open usually means selecting, not running
-            # Can be untrue, but it's better than the alternative
-            if not commandline --paging-mode
-                set -g _tide_transient
-                set -g _tide_repaint
-                commandline -f repaint
-            end
+        # If the commandline will be executed or is empty, and the pager is not open
+        # Pager open usually means selecting, not running
+        # Can be untrue, but it's better than the alternative
+        if commandline --is-valid || test -z "$(commandline)" && not commandline --paging-mode
+            set -g _tide_transient
+            set -g _tide_repaint
+            commandline -f repaint
         end
         commandline -f execute
     end
